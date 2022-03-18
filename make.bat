e3d92f8b2049de47cf390e8a46bbbe96358e6f1f
Don Gochenour <don@rti.com>
2022-03-18 13:01:58 -0400

    fix linux - Win10 differences

--- make.bat
+++ make.bat
@@ -16,26 +16,26 @@ echo 01 > demoCA\serial
 %OPENSSL_EXE% req -new -key demoCA\private\cakey.pem -out ca.csr -config input\openssl.cnf
 %OPENSSL_EXE% x509 -req -days 3650 -in ca.csr -signkey demoCA\private\cakey.pem -out demoCA\cacert.pem
 REM Generate private key for CA
-%OPENSSL_EXE% genrsa -out HelloSecurePublisher_key.pem 2048
+%OPENSSL_EXE% genrsa -out publisher_private_key.pem 2048
 REM Create Certificate request
-%OPENSSL_EXE% req -config input\HelloSecurePublisher.cnf -new -key HelloSecurePublisher_key.pem -out temp.csr
+%OPENSSL_EXE% req -config input/publisher.cnf -new -key publisher_private_key.pem -out temp.csr
 REM Sign Certificate request for Domain Participant by CA
-%OPENSSL_EXE% ca -days 365 -in temp.csr -out HelloSecurePublisher_cert.pem
+%OPENSSL_EXE% ca -days 365 -in temp.csr -out publisher_cert.pem
 REM Create Permissions Grant File for Domain Participant
 REM Sign Permissions Document
-%OPENSSL_EXE% smime -sign -in input\HelloSecurePublisher_permissions.xml -text -out signed_HelloSecurePublisher_permissions.xml -signer demoCA\cacert.pem -inkey demoCA\private\cakey.pem
+%OPENSSL_EXE% smime -sign -in input\publisher_permissions.xml -text -out signed_publisher_permissions.xml -signer demoCA\cacert.pem -inkey demoCA\private\cakey.pem
 REM generate private key for CA
-%OPENSSL_EXE% genrsa -out HelloSecureSubscriber_key.pem 2048
+%OPENSSL_EXE% genrsa -out subscriber_private_key.pem 2048
 REM Create Certificate request
-%OPENSSL_EXE% req -config input\HelloSecureSubscriber.cnf -new -key HelloSecureSubscriber_key.pem -out temp.csr
+%OPENSSL_EXE% req -config input\subscriber.cnf -new -key subscriber_private_key.pem -out temp.csr
 REM Sign Certificate request for Domain Participant by CA
-%OPENSSL_EXE% ca -days 365 -in temp.csr -out HelloSecureSubscriber_cert.pem
+%OPENSSL_EXE% ca -days 365 -in temp.csr -out subscriber_cert.pem
 REM Create Permissions Grant File for Domain Participant
 REM Sign Permissions Document
-%OPENSSL_EXE% smime -sign -in input\HelloSecureSubscriber_permissions.xml -text -out signed_HelloSecureSubscriber_permissions.xml -signer demoCA\cacert.pem -inkey demoCA\private\cakey.pem
+%OPENSSL_EXE% smime -sign -in input\subscriber_permissions.xml -text -out signed_subscriber_permissions.xml -signer demoCA\cacert.pem -inkey demoCA\private\cakey.pem
 REM Sign governance file
-%OPENSSL_EXE% smime -sign -in input\Governance.xml -text -out signed_Governance.xml -signer demoCA\cacert.pem -inkey demoCA\private\cakey.pem
+%OPENSSL_EXE% smime -sign -in input\governance.xml -text -out signed_governance.xml -signer demoCA\cacert.pem -inkey demoCA\private\cakey.pem
 
-COPY signed*.xml output\ & COPY HelloSecure*.pem output\ & COPY demoCA\cacert.pem output\
+MOVE signed_* output\ & MOVE publisher_* output\ & MOVE subscriber_* output\ & MOVE *_private_key.pem output & COPY demoCA\cacert.pem output\
 
 echo "Done."
